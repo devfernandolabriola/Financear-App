0bbfd48545889c0af64ca804b4e9dd870bd1365e
HaiseCoffee <frantorres1806@gmail.com>
2025-01-06 20:48:26 -0300

    signup_user

---
+++ Finanzas/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Finanzas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
