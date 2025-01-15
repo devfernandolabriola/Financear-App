bff26d9171be268b9dc86c2fca180711fc42d067
ferlabriola <ferlabriola@gmail.com>
2025-01-15 01:20:01 -0300

    se termino el sign up

--- Finanzas/Controllers/UsuarioController.cs
+++ Finanzas/Controllers/UsuarioController.cs
@@ -1,6 +1,9 @@
-﻿using Finanzas.Models;
+﻿using Finanzas.DTO;
+using Finanzas.Models;
 using Finanzas.Models.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -26,36 +29,33 @@ namespace Finanzas.Controllers
 
         [HttpPost]
         [Route("register")]
-        public ActionResult Register(string Email, string Nombre, string Clave)
+        public IActionResult Register([FromBody] UsuarioDTO userDTO/*string Email*//*, string Nombre, string Clave*/)
         {
-            var resultado = VerificarCorreo(Email);
+            var resultado = VerificarDatosRegister(userDTO.Email, userDTO.Nombre);
             if (resultado == 422)
             {
-                return StatusCode(422);
+                return Unauthorized("Feo");
             }
-            var ClaveCifrada = HashHelper.HashPassword(Clave);
-            var success = Usuario.RegisterUser(_context, Email, Nombre, ClaveCifrada);
+            var ClaveCifrada = HashHelper.HashPassword(userDTO.Clave);
+            var success = Usuario.RegisterUser(_context, userDTO.Email, userDTO.Nombre, ClaveCifrada);
             //if (success) return View("Success"); else return View("Fail");
             if (success)
             {
-                var datoUsuario = _context.Usuarios.First(x => x.Email == Email);
-                return RedirectToAction("Index", "Home", new
-                {
-                    id = datoUsuario.Id
-                });
-            }
-            return StatusCode(500);
-        } 
-        
+                var datoUsuario = _context.Usuarios.First(x => x.Email == userDTO.Email);
+                return Json(new { success = true, redirect = Url.Action("Index", "Home", new { id = datoUsuario.Id }) });
+            };
 
-        public int VerificarCorreo(string Email)
+            return Json(new { success = false, message = "Ocurrió un error al registrar el usuario" });
+        }
+        
+        public int VerificarDatosRegister(string Email, string nombre)
         {
-            if (string.IsNullOrEmpty(Email))
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(nombre))
             {
                 return 400;
             }
-            var usuario = _context.Usuarios.FirstOrDefault(x => x.Email == Email);
-            if (usuario != null)
+            var usuario = _context.Usuarios.FirstOrDefault(x => x.Email == Email || x.Nombre == nombre);
+            if (usuario != null )
             {
                 return 422;
             }
