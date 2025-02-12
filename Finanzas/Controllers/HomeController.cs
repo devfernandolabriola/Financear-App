350bab661b6314110b149f53d03d25dc4acb4091
HaiseCoffee <frantorres1806@gmail.com>
2025-02-12 00:06:09 -0300

    fixeo sesion en layout

--- Finanzas/Controllers/HomeController.cs
+++ Finanzas/Controllers/HomeController.cs
@@ -25,7 +25,6 @@ namespace Finanzas.Controllers
             ViewBag.IsUserLoggedIn = HttpContext.Session.GetString("UsuarioId") != null;
             if(HttpContext.Session.GetString("UsuarioId") != null)
             {
-                ViewBag.NombreUsuario = HttpContext.Session.GetString("UsuarioNombre");
                 ViewBag.TieneCuentas = false;
                 bool tieneCuentas = Usuario.VerificarCuentasVinculadasUsuario(_context, int.Parse(HttpContext.Session.GetString("UsuarioId")));
                 if (tieneCuentas)
